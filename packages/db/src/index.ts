d8dd9a7e46fd86c56acd5c76cb7f8e7fd52135e3
SrKtym <frmywrkplc@gmail.com>
2026-06-02 19:21:01 +0900

    fix: DBクライアントを生成する関数の修正

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,11 +1,14 @@
-import { env } from "@lms-repo-edge-version/env/server";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
 
 export function createDb() {
+	const connectionString = process.env.DATABASE_URL;
+	if (!connectionString) {
+		throw new Error("DATABASE_URL is not set");
+	}
 	const pool = new Pool({
-		connectionString: env.DATABASE_URL,
+		connectionString,
 		maxUses: 1,
 	});
 	return drizzle(pool, { schema });
