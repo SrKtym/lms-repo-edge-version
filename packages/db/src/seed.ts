5bba3f65192fb213a60cb7de4222cd2f7f4e70dc
SrKtym <frmywrkplc@gmail.com>
2026-06-02 19:23:11 +0900

    ローカル開発環境（nodejs）でデータベースにシーディングできるように修正

--- packages/db/src/seed.ts
+++ packages/db/src/seed.ts
@@ -1,3 +1,19 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import dotenv from "dotenv";
+
+const envResult = dotenv.config({
+	path: join(__dirname, "../../../apps/server/.env.development"),
+});
+
+if (envResult.error) {
+	throw envResult.error;
+}
+
 import { seedCourseData } from "./seed/seed-course-data";
 import { seedUserData } from "./seed/seed-prof-data";
 
